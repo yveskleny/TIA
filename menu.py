bd62c75bba3ea5c4af537876990885864e6c37e3
Yves Kleny <48736725+yveskleny@users.noreply.github.com>
2025-09-03 00:41:41 +0000

    menu

---
+++ menu.py
@@ -0,0 +1,9 @@
+opcao = ''
+
+while opcao != '3':
+    print("Menu de opções: ")
+    print("1 - Ver Saldo")
+    print("2 - Depósito")
+    print("3 - Sair")
+    opcao = input("Qual opção você deseja? ")
+    print()
